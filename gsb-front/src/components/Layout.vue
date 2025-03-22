433f00176da53423fbf8f630f1c418a710731f51
Pasdinspy <gabriel.kerfourn@gmail.com>
2025-03-22 10:24:36 +0000

    modification du css

--- gsb-front/src/components/Layout.vue
+++ gsb-front/src/components/Layout.vue
@@ -1,24 +1,12 @@
 <template>
-    <div class="layout-container">
-      <Sidebar />
-      <div class="content-container">
-        <slot></slot>
-      </div>
+  <div class="layout-container">
+    <Sidebar />
+    <div class="content-container">
+      <slot></slot>
     </div>
-  </template>
-  
-  <script setup>
-  import Sidebar from './Sidebar.vue'
-  </script>
-  
-  <style scoped>
-  .layout-container {
-    display: flex;
-    flex-direction: row;
-  }
-  
-  .content-container {
-    flex: 1;
-    padding: 2rem;
-  }
-  </style>
+  </div>
+</template>
+
+<script setup>
+import Sidebar from './Sidebar.vue'
+</script>
